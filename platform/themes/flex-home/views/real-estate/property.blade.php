dfb6e8d05b83d037ac8cd37963339c656e0cf810
ZK <zk27159@gmail.com>
2022-05-09 13:38:43 +0500

    .

--- platform/themes/flex-home/views/real-estate/property.blade.php
+++ platform/themes/flex-home/views/real-estate/property.blade.php
@@ -24,8 +24,8 @@ $state = \Botble\Location\Models\State::with(['cities.properties'])
   }
 </style>
 
-<!-- {!! Theme::header() !!}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> -->
+{!! Theme::header() !!}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
 
 <div class="w-100">
