6be99b8e7090a6e1bd662e77a1d895c76b6157e5
ZK <zk27159@gmail.com>
2022-05-09 15:45:36 +0500

    .

--- platform/themes/flex-home/views/real-estate/property.blade.php
+++ platform/themes/flex-home/views/real-estate/property.blade.php
@@ -20,6 +20,7 @@ $state = \Botble\Location\Models\State::with(['cities.properties'])
 </style>
 
 {!! Theme::header() !!}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
 
 <div class="w-100">
