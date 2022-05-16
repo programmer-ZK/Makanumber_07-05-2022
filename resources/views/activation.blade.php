81e5a3ca2ce4f8c8c1c44f7690d8f0850e7a0438
ZK <zk27159@gmail.com>
2022-05-16 18:52:18 +0500

    .

--- resources/views/activation.blade.php
+++ resources/views/activation.blade.php
@@ -1,42 +1,34 @@
-<style>
-  .container{
-    color: red;
-  }
-</style>
-
 <h1>
   Hello {{ $name }}, <br>
 </h1>
-<div class="container">
 
-  <p>
-    We would like to thank you for choosing Makanumber.com <br>
+<p>
+  We would like to thank you for choosing Makanumber.com <br>
 
-    Please click on the link below to activate your account and get started.
-  </p>
-
+  Please click on the link below to activate your account and get started.
+</p>
 
 
-  <a href="{{ $body }}" target="_blank">Click To Activation</a>
 
-  <p>Want to know more about Makanumber</p>
+<a href="{{ $body }}" target="_blank">Click To Activation</a>
+
+<p>Want to know more about Makanumber</p>
 
 
+<br>
+<p>
+  Visit <a href="www.makanumber.com">www.makanumber.com</a>
+</p>
+
+<br>
+<p>Thankyou for your time.</p>
+
+<br>
+
+
+<br>
+<p>
+  Regards,
   <br>
-  <p>
-    Visit <a href="www.makanumber.com">www.makanumber.com</a>
-  </p>
-
-  <br>
-  <p>Thankyou for your time.</p>
-
-  <br>
-
-
-  <br>
-  <p>
-    Regards,
-    <br>
-    Team MKNB
-  </p>
-</div>
+  Team MKNB
+</p>
